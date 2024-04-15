5bd42759f4956273397fadf977491098d9e58290
Talib <mirzatalib110@gmail.com>
2024-04-15 18:52:51 -0500

    heyyy

--- uiucforum/src/data.py
+++ uiucforum/src/data.py
@@ -1,4 +1,14 @@
 import csv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf import FlaskForm
+
 
 csv_file = r'C:\Users\Mirza\Downloads\course-catalog.csv'
 def extract(csv_file):
@@ -16,7 +26,6 @@ def extract(csv_file):
 unique = extract(csv_file)
 uniquesort = sorted(unique)
 
-print(uniquesort)
 
 def convert_set_to_csv(grok_set, output_file):
     fieldnames = ['Class', 'Number', 'Name']
@@ -25,13 +34,41 @@ def convert_set_to_csv(grok_set, output_file):
     with open(output_file, mode='w', newline='') as file:
         writer = csv.writer(file)
         
-        # Write header
         writer.writerow(fieldnames)
         
-        # Write set elements into CSV file, filling missing columns with empty strings
         for row in grok_set:
-            writer.writerow(row[:3])  # Take first three elements of each set row
-
+            writer.writerow(row[:3])
 
 output_csv_file = 'class_data.csv'
 convert_set_to_csv(uniquesort, output_csv_file)
+
+Base = declarative_base()
+
+engine = create_engine('sqlite:///database.db', echo=True)
+
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+session.commit()
+
+results = session.query(User).all()
+
+print(results)
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max = 30)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max = 30)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max = 30)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('login')
